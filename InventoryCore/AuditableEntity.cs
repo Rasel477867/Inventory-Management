84b91e6fdd46672eaf4072addd811f20f8293443
“Rasel477867” <raselaye3737@gamil.com>
2024-09-28 07:35:54 +0600

    Entity design updated.

---
+++ InventoryCore/AuditableEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryCore
+{
+    public class AuditableEntity:BaseEntity
+    {
+        public Guid? LastModifiedBy { get; set; }
+
+        public DateTime? LastModified { get; set; }
+        public Guid? CreatedBy { get; set; }
+
+        public DateTime Created { get; set; }
+
+
+        public AuditableEntity()
+        {
+            Created = DateTime.Now;
+        }
+    }
+}
---
+++ InventoryCore/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryCore
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+
+        public BaseEntity()
+        {
+            this.Id = Guid.NewGuid();
+            this.IsDeleted = false;
+            this.IsActive = true;
+        }
+    }
+}
